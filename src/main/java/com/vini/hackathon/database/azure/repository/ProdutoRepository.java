f000a90ae90da8142ac376a4331b9f166dd57e67
c159122 <c159122@corp.caixa.gov.br>
2025-08-20 16:10:33 -0300

    cria query para busca de produto via parâmetros

--- src/main/java/com/vini/hackathon/database/azure/repository/ProdutoRepository.java
+++ src/main/java/com/vini/hackathon/database/azure/repository/ProdutoRepository.java
@@ -2,9 +2,14 @@ package com.vini.hackathon.database.azure.repository;
 
 import com.vini.hackathon.database.azure.entity.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+    @Query("SELECT p FROM Produto p WHERE p.nuMinimoMeses <= :prazo AND p.nuMaximoMeses >= :prazo AND p.vrMinimo <= :valor AND p.vrMaximo >= :valor")
+    Produto buscarProduto(@Param("valor") double valor, @Param("prazo") int prazo);
 
 }
