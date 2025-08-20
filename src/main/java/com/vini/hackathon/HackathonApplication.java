e626db97159422dd8c9eb8d6b96b085ead1d2fdc
Vinícius Oliveira <vo70346@gmail.com>
2025-08-19 22:27:17 -0300

    configura uso dos databases

--- src/main/java/com/vini/hackathon/HackathonApplication.java
+++ src/main/java/com/vini/hackathon/HackathonApplication.java
@@ -2,9 +2,14 @@ package com.vini.hackathon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@EnableJpaRepositories(
+		basePackages = "com.vini.hackathon.database.azure.repository"
+)
+@EntityScan(basePackages = "com.vini.hackathon.database.azure.entity")
+@SpringBootApplication
 public class HackathonApplication {
 
 	public static void main(String[] args) {
