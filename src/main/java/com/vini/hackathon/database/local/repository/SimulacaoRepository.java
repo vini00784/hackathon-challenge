d5b05f826be2d2814b0d0ce814d24ea2339b426f
c159122 <c159122@corp.caixa.gov.br>
2025-08-22 15:13:05 -0300

    adiciona paginação na busca das simulações

--- src/main/java/com/vini/hackathon/database/local/repository/SimulacaoRepository.java
+++ src/main/java/com/vini/hackathon/database/local/repository/SimulacaoRepository.java
@@ -1,9 +1,17 @@
 package com.vini.hackathon.database.local.repository;
 
 import com.vini.hackathon.database.local.entity.Simulacao;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SimulacaoRepository extends JpaRepository<Simulacao, Integer> {
+
+    @Override
+    @NonNull
+    Page<Simulacao> findAll( @NonNull Pageable pageable);
+
 }
