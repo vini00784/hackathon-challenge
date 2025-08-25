34d306cc04cbbc83b35cd5638977742cdba3e376
Vinícius Oliveira <vo70346@gmail.com>
2025-08-24 22:14:38 -0300

    cria função para busca de simulação por data e produto

--- src/main/java/com/vini/hackathon/database/local/repository/SimulacaoRepository.java
+++ src/main/java/com/vini/hackathon/database/local/repository/SimulacaoRepository.java
@@ -4,8 +4,13 @@ import com.vini.hackathon.database.local.entity.Simulacao;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface SimulacaoRepository extends JpaRepository<Simulacao, Integer> {
@@ -13,5 +18,8 @@ public interface SimulacaoRepository extends JpaRepository<Simulacao, Integer> {
     @Override
     @NonNull
     Page<Simulacao> findAll( @NonNull Pageable pageable);
+
+    @Query("SELECT s FROM Simulacao s WHERE s.codigoProduto = :codigoProduto AND s.createdAt = :dataReferencia")
+    List<Simulacao> listarPorDataEProduto(@Param("codigoProduto") int codigoProduto, @Param("dataReferencia") LocalDate dataReferencia);
 
 }
